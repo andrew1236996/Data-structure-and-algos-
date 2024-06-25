d43d87a988eb970ad47fd5398e29b8378f22fb89
andrew1236996 <93327556+andrew1236996@users.noreply.github.com>
2024-06-25 03:32:48 +0100

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[]args) {
+
+        Stack stack = new Stack(10);
+        stack.push(56);
+        stack.push(45);
+        stack.push(5);
+        stack.push(15);
+        stack.printStack();
+    }
+}
